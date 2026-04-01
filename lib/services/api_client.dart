08d5f46f3ea5ca0d2bb559c9448a3bd9c5aebfb4
seanr89 <srafferty89@gmail.com>
2026-04-01 20:14:03 +0100

    chore: update baseUrl to point to production Azure API endpoint

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -15,7 +15,8 @@ class ApiException implements Exception {
 class ApiClient {
   // Use Mac's local IP for testing on physical device
   // Change back to 'http://localhost:3000' when using simulator
-  static const String baseUrl = 'http://192.168.1.203:3000';
+  static const String baseUrl =
+      'https://flixie-api-fmcehvaecwdheccm.northeurope-01.azurewebsites.net';
 
   // static const String baseUrl =
   //     'https://flixibe.graysea-314b1b40.northeurope.azurecontainerapps.io';
